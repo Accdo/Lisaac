c8d2f23144bfa2e796ad0d3cb6cbc3a8ba86da02
Eoryeong <wlsdyd058@gmail.com>
2025-03-28 11:39:55 +0900

    Apollo Update

--- Assets/Scripts/Enemy/BossExoMech/ApolloFireBall.cs
+++ Assets/Scripts/Enemy/BossExoMech/ApolloFireBall.cs
@@ -5,12 +5,14 @@ public class ApolloFireBall : MonoBehaviour
 
     public float speed = 5f;
     public float lifeTime = 3f;
-    public int damage = 10;
-    public Vector2 direction;
+    public int damage = 1;
+    private Vector2 direction;
+
+    public GameObject fireBallMini;
 
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+        Invoke("ExplosionFireball", lifeTime);
     }
 
     public void SetDirection(Vector2 dir)
@@ -30,6 +32,20 @@ public class ApolloFireBall : MonoBehaviour
             // 플레이어 데미지 로직
             Destroy(gameObject);
         }
+    }
+
+    void ExplosionFireball()
+    {
+        Vector3 vector = new Vector3(0f, 0f, 45f);
+        GameObject missile1 = Instantiate(fireBallMini, transform.position, Quaternion.Euler(vector));
+        vector.z += 90;
+        GameObject missile2 = Instantiate(fireBallMini, transform.position, Quaternion.Euler(vector));
+        vector.z += 90;
+        GameObject missile3 = Instantiate(fireBallMini, transform.position, Quaternion.Euler(vector));
+        vector.z += 90;
+        GameObject missile4 = Instantiate(fireBallMini, transform.position, Quaternion.Euler(vector));
+
+        Destroy(gameObject);
     }
 
 }
---
+++ Assets/Scripts/Enemy/BossExoMech/ApolloMissile.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ApolloMissile : MonoBehaviour
+{
+
+    public float speed = 2f;
+    public float lifeTime = 3f;
+    public int damage = 1;
+
+    private Transform playerLocation;
+
+    void Start()
+    {
+        playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
+        Invoke("SelfDestruct", lifeTime);
+    }
+
+    void Update()
+    {
+        Vector2 direction = (playerLocation.position - transform.position).normalized; // 플레이어 방향 계산
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // 각도로 변환
+
+        // 시선이 위쪽이여서 -90도를 하여 플레이어를 바라보게함
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+    }
+
+    void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
